0bd445075df871a7131e6dfcdf3f7f16872248a0
Hamid Mehmood <HamidQureshi@users.noreply.github.com>
2019-01-04 11:53:09 +0000

    Update Transaction.rb
    adding comments

--- Transaction.rb
+++ Transaction.rb
@@ -1,5 +1,6 @@
 class Transaction
 
+  # function requires (keyname , public key and type of the encryption) and creates a transaction JSON object
   def buildTxObject(keyName, publicKey, type)
 
     tx_obj ={
@@ -16,7 +17,8 @@ class Transaction
     return tx_obj
   end
 
-
+  # function requires (keyname , public key, signature of transaction object and type of the encryption) and creates an onboard
+  #  transaction JSON object
   def buildOnboardTransaction(keyName, publicKey, type, signature)
 
     #generate transaction
