8d53f93750db9e1c2b9dcbe9b79dd36437c394e2
Hamid Mehmood <HamidQureshi@users.noreply.github.com>
2019-01-04 11:49:43 +0000

    Update PreferenceUtils.rb
    adding comments

--- PreferenceUtils.rb
+++ PreferenceUtils.rb
@@ -6,6 +6,8 @@ class PreferenceUtils
 
   $url = "http://testnet-uk.activeledger.io:5260"
 
+  # setting up connection for http request
+  # requires (protocol, url and port) as an input parameters and convert them into url
   def setConnection(protocol, url ,port)
     $url =  "#{protocol}://#{url}:#{port}"
   end
@@ -18,9 +20,13 @@ class PreferenceUtils
     return json.to_json
   end
 
+  # function takes filename and key as an input, writes the key into the file and saves the file to current directory
   def writeKeyInFile(filename,key)
+    
+    # change the directory to the root of the project
     Dir.chdir(File.dirname(__FILE__))
 
+    # creates a new file with writing privilages
     aFile = File.new(filename, "w+")
 
     if aFile
@@ -29,6 +35,7 @@ class PreferenceUtils
       puts "Unable to open file!"
     end
 
+    # closes the file stream
     aFile.close
   end
 
